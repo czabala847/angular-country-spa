5bd7a408b3961727685cfe0401923b6e8a0afe93
Carlos Zabala <czabala847@gmail.com>
2023-08-22 19:51:00 -0500

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing-module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule } from '@angular/router';
 
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SideBarComponent,
+    ContactPageComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [SideBarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
