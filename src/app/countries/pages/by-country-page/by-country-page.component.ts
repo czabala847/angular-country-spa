f49372eeed5acbfe3dbd607889948e69205f26fa
Carlos Zabala <czabala847@gmail.com>
2023-08-22 22:23:18 -0500

    search by params

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ByCountryPageComponent {
+  public countries: Country[] = [];
 
+  constructor(private countriesService: CountriesService) {}
+
+  onNewValue(term: string) {
+    this.countriesService
+      .searchByCountry(term)
+      .subscribe((countries) => (this.countries = countries));
+  }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -11,10 +11,34 @@ export class CountriesService {
 
   constructor(private http: HttpClient) {}
 
+  searchCountries(urlFetch: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(urlFetch)
+      .pipe(catchError((error) => of([])));
+  }
+
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const URL_FETCH = `${this.BASE_URL}/alpha/${code}`;
+    return this.searchCountries(URL_FETCH).pipe(
+      map((countries) => {
+        if (countries.length === 0) return null;
+        return countries[0];
+      })
+    );
+  }
+
   searchByCapital(term: string): Observable<Country[]> {
     const URL_FETCH = `${this.BASE_URL}/capital/${term}`;
-    return this.http
-      .get<Country[]>(URL_FETCH)
-      .pipe(catchError((error) => of([])));
+    return this.searchCountries(URL_FETCH);
+  }
+
+  searchByRegion(term: string): Observable<Country[]> {
+    const URL_FETCH = `${this.BASE_URL}/region/${term}`;
+    return this.searchCountries(URL_FETCH);
+  }
+
+  searchByCountry(term: string): Observable<Country[]> {
+    const URL_FETCH = `${this.BASE_URL}/name/${term}`;
+    return this.searchCountries(URL_FETCH);
   }
 }
